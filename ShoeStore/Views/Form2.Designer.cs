31eacdd8ea132746a07e4e5146be3b655aa446fc
vunguyen2201 <vugnuyen22271@gmail.com>
2020-12-28 21:10:49 +0700

    add form 2

--- ShoeStore/Views/Form2.Designer.cs
+++ ShoeStore/Views/Form2.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace ShoeStore.Views
 {
-    partial class Form1
+    partial class Form2
     {
         /// <summary>
         /// Required designer variable.
@@ -32,7 +32,7 @@ namespace ShoeStore.Views
             this.components = new System.ComponentModel.Container();
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Text = "Form2";
         }
 
         #endregion
